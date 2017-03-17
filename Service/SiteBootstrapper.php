f56f46502227b3254fad93e81a5baad25e584fe0
David Yang <daworld.ny@gmail.com>
2017-03-17 11:13:12 +0500

    Added site bootstrapper

---
+++ Service/SiteBootstrapper.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Members\Service;
+
+use Cms\Service\AbstractSiteBootstrapper;
+
+final class SiteBootstrapper extends AbstractSiteBootstrapper
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function bootstrap()
+    {
+        $this->view->addVariable('member', $this->moduleManager->getModule('Members')->getService('siteService'));
+    }
+}
